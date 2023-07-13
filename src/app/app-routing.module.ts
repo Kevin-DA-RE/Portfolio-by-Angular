78a953ebe0f60a3f5fa5993ab643e3695fa07bd3
Kevin DA-RE <dare.kevin21000@gmail.com>
2023-07-13 15:42:37 +0200

    Mise en place des routeurs pour Experience

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BiographieComponent } from './biographie/biographie.component';
+import { ExperienceListComponent } from './experience-list/experience-list.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
-  {path: 'about', component: BiographieComponent}
+  {path: 'about', component: BiographieComponent},
+  {path: 'experience', component: ExperienceListComponent}
 ];
 
 @NgModule({
