18c476a39b57bb36e6bd2928d78916e90173f3ab
Kevin DA-RE <dare.kevin21000@gmail.com>
2023-07-27 23:25:24 +0200

    Suppression routeur

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { BiographieComponent } from './biographie/biographie.component';
-import { ExperienceListComponent } from './experience-list/experience-list.component';
-import { CompetenceComponent } from './competence/competence.component';
-import { ProjetListComponent } from './projet-list/projet-list.component';
 
-const routes: Routes = [
-  {path:'', component: HomeComponent},
-  {path: 'competence', component: CompetenceComponent},
-  {path: 'about', component: BiographieComponent},
-  {path: 'experience', component: ExperienceListComponent},
-  {path: 'projet', component: ProjetListComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
